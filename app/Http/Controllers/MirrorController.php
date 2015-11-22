f14bad55246fc8f36e708f617a088f2746edc905
Ash White <ash@theadsmith.com>
2015-11-21 20:51:29 -0500

    Added a few test cities

--- app/Http/Controllers/MirrorController.php
+++ app/Http/Controllers/MirrorController.php
@@ -12,8 +12,11 @@ class MirrorController extends Controller
     public function index()
     {
         $weather_controller = new WeatherController();
-        $weather_data = $weather_controller->get_weather(33.9567, -83.3583);
-        // $weather_data = $weather_controller->get_weather(34.0500, -118.2500);
+        $weather_data = $weather_controller->get_weather(33.9567, -83.3583); // Athens
+        // $weather_data = $weather_controller->get_weather(34.0500, -118.2500); // LA
+        // $weather_data = $weather_controller->get_weather(40.7142, -74.0064); // NY
+        // $weather_data = $weather_controller->get_weather(51.5171, -0.1062); // London
+        // $weather_data = $weather_controller->get_weather(-33.8683, 151.2086); // Sydney
 
         $locale = 'Athens, GA';
         $date = date('l, F d, Y');
