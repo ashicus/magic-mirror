914748b62ff7614250dcbcb653b925c240a94d92
Ash White <ash@theadsmith.com>
2015-11-28 18:13:22 -0500

    Tweaked layout

--- public/js/home.js
+++ public/js/home.js
@@ -1,5 +1,5 @@
 (function($) {
-    var refresh_interval = 30 * 1000;
+    var refresh_interval = 5 * 60000;
     var refresh_timer = setInterval(function() {
         location.reload();
     }, refresh_interval);
