6d508adc80e8338d3ea35347b1dbe2087c484c64
Ash White <ash.white@x-team.com>
2018-01-07 22:05:59 -0500

    Tweaked refresh code because I forgot how time works

--- public/js/home.js
+++ public/js/home.js
@@ -1,5 +1,6 @@
 (function($) {
-    var refresh_interval = 5 * 60000;
+    var refresh_interval_miuntes = 5;
+    var refresh_interval = refresh_interval_miuntes * 60000;
     var refresh_timer = setInterval(function() {
         location.reload();
     }, refresh_interval);
